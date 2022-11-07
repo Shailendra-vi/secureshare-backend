b42835c9fdfe84b181ad309a03ae9128a123672e
Shailendra-vi <shailendrasv0110@gmail.com>
2022-11-07 22:08:55 +0530

    test2

--- src/main/java/com/backend/secureshare/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.backend.secureshare.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.backend.secureshare.entities.User;
-
-public interface UserRepo extends JpaRepository<User,Integer>{
-
-}
